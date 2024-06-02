284d8647c97aba0ec7724a023ab6210024843de0
Pavel Pavlov <ppavvvlov@gmail.com>
2024-06-03 02:21:41 +0300

    frendip: добавил кастомный маркер подстанции

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MapLayout from './components/MapLayout';
+import MapSubstationMarker from './components/MapSubstationMarker';
 
 import './styles/style.css';
 
@@ -6,7 +7,9 @@ function App() {
     return (
         <div className="flex gap-x-4">
             <div className="h-50v w-50v">
-                <MapLayout></MapLayout>
+                <MapLayout>
+                    <MapSubstationMarker coordinates={[37.623082, 55.75254]} />
+                </MapLayout>
             </div>
 
             <div>OOOPS</div>
---
+++ src/components/MapSubstationMarker.tsx
@@ -0,0 +1,26 @@
+import type {YMapMarkerProps} from '../lib/ymaps';
+import {YMapMarker} from '../lib/ymaps';
+import {ReactComponent as SubstationIcon} from '../assets/substation-icon.svg';
+import React from 'react';
+
+interface MapSubstationMarkerProps extends YMapMarkerProps {
+    status?: 'working';
+}
+
+const objectStatus = {
+    working: 'border-[#5DE763]'
+};
+
+function MapSubstationMarker({status = 'working', ...markerProps}: MapSubstationMarkerProps) {
+    return (
+        <YMapMarker {...markerProps}>
+            <div
+                className={`relative flex -translate-x-2/4 -translate-y-2/4 items-center justify-center rounded-full border-4 ${objectStatus[status]} bg-yellow-200 p-1`}
+            >
+                <SubstationIcon />
+            </div>
+        </YMapMarker>
+    );
+}
+
+export default MapSubstationMarker;
