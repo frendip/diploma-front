796a302e74cb25dc3deaaf17b16a93ad79317f37
bakug <bakugan_a2002@mail.ru>
2024-06-17 15:21:17 +0300

    Andrey: добавил поиск на панель подстанций

--- src/components/Substations/Panel.tsx
+++ src/components/Substations/Panel.tsx
@@ -1,5 +1,6 @@
 import {useCallback} from 'react';
 import {Link} from 'react-router-dom';
+import {ReactComponent as SearchIcon} from '../../assets/search-icon.svg';
 import {useAppDispatch} from '../../hooks/useAppDispatch';
 import {useAppSelector} from '../../hooks/useAppSelector';
 import {setActiveSubstation} from '../../store/slices/vinaigretteSlice';
@@ -40,6 +41,20 @@ function Panel({className: externalStyles}: PanelProps) {
                         <CommonButton text={'Добавить подстанцию'} />
                     </Link>
                 )}
+                <div>
+                    <label className="relative block">
+                        <span className="sr-only">Search</span>
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <SearchIcon className="h-5 w-5" />
+                        </span>
+                        <input
+                            className="block w-full rounded-lg border border-slate-300 bg-white py-1 pl-9 pr-3 shadow-sm placeholder:text-slate-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+                            placeholder="Поиск..."
+                            type="text"
+                            name="search"
+                        />
+                    </label>
+                </div>
             </div>
             {panelActiveType === 'substations' ? <List /> : <BasesList />}
         </div>
